4aa3e99d466a58b37b31c4058e510a4b1d931c3f
Willie Thean <willie.thean@workday.com>
2017-12-12 14:45:58 -0800

    Update file

--- MinimumOf2Num.js
+++ MinimumOf2Num.js
@@ -11,3 +11,33 @@ function Minimum (num1, num2) {
 
 console.log(Minimum(5, 10));
 console.log(Minimum(4, 3));
+
+var str = "Hello";
+console.log(str.toUpperCase);
+console.log(str.toUpperCase());
+
+console.log(typeof[1, 2]);
+
+var array = ["Hello", "World", "Show", "Me", "Your", "Leader"]
+console.log(array.indexOf("Your"));
+
+var countArg = function() {
+	console.log('Number of arguments are : ' + arguments.length);
+	console.log('They are : ');
+	for (var i = 0; i < arguments.length; i++) {
+		console.log(arguments[i]);
+	}
+}
+
+countArg("Having", "Fun", "learning", 1, 2, 3);
+
+function forEach(array, action) {
+	for (var i = 0; i < array.length; i++) {
+		action(array[i]);
+	}
+}
+
+var myArray = ["Hi", 1, "Hello"];
+forEach(myArray, function(arg) {
+	console.log(arg);
+});
